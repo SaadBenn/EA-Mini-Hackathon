93c56c379b5b1d299564d7a129177bd77768c0f0
SaadBenn <saadbennmushtaq@gmail.com>
2019-08-09 08:52:22 -0700

    Demo async iterators

---
+++ async_iterators.js
@@ -0,0 +1,29 @@
+const fetch = require('node-fetch');
+async function streamingRead() {
+  const response = await fetch('https://www.reddit.com/r/javascript/top/.json?limit=5');
+  console.log(response.body);
+  for await (const chunk of streamAsyncIterator(response.body)) {
+    console.log(`Read ${chunk.length} bytes`);
+  }
+}
+
+async function* streamAsyncIterator(stream) {
+  // lock the stream
+  const reader = stream.getReader();
+
+  try {
+    while (true) {
+      // Read from the stream
+      const {done, value} = await reader.read();
+      // Exit if we're done
+      if (done) return;
+      // Else yield the chunk
+      yield value;
+    }
+  }
+  finally {
+    reader.releaseLock();
+  }
+}
+
+streamingRead();
